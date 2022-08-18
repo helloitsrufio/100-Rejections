a202fb338bcb739dee0498895c7096768ddfeb06
Ruth Reed <ruthreed.dev@gmail.com>
2022-08-18 15:28:29 -0600

    Add files as of 8/16/22.

---
+++ pages/about.jsx
@@ -0,0 +1,23 @@
+import Navbar from "../components/navbar";
+
+
+export default function About() {
+  return (
+    <>
+      <Navbar/>
+      <section>
+        <img></img>
+        <h2>About</h2>
+        <p>On the job hunt? Love checklists? This app is for you.</p>
+        <p>Track how many companies have rejected you on the job search.</p>
+        <p>
+          The hope is that getting those 100 stars will motivate you to apply
+          and interview for many more jobs than you would have otherwise. And
+          though you’re racking up rejections, every no leads you closer to that
+          one yes. It is our hope that you won’t ever get 100 stars; you’ll have
+          found a job first.
+        </p>
+      </section>
+    </>
+  );
+}
