ca2eb56dca92f4b961d62f622d0f77729d8f1cd8
Ruth Reed <ruthreed.dev@gmail.com>
2023-01-24 16:12:43 -0700

    Add styling for stars on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,8 +17,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div>{starArray.map(e => ({...e, key: nanoid()}))}</div>
+      <main className='grid place-content-center'>
+        <div className="flex flex-wrap justify-center pt-12 px-1 md:max-w-xl lg:max-w-3xl">{starArray.map(e => ({...e, key: nanoid()}))}</div>
       </main>
     </>
   )
